d3e0c5383f63b9a3a38973584a218f3c02c4be6e
chirag_k_poojary <chiragkpoojary30@gmail.com>
2024-11-08 22:18:30 +0530

    mstyling chnages

--- src/Components/Animated-menu.jsx
+++ src/Components/Animated-menu.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { MotionConfig, motion } from "framer-motion";
 
-export  const Example = () => {
+export const Example = () => {
   return (
-    <div >
+    <div>
       <AnimatedHamburgerButton />
     </div>
   );
@@ -25,17 +25,14 @@ const AnimatedHamburgerButton = () => {
         className="relative h-20 w-20 rounded-full bg-white/0 transition-colors hover:bg-white/20"
       >
         <motion.span
-         
           className="absolute h-[0.3rem] w-10 bg-gradient-to-r from-cyan-500 to-blue-500 ... rounded-xl"
           style={{ y: "-50%", left: "50%", x: "-50%", top: "35%" }}
         />
         <motion.span
-        
           className="absolute h-[0.3rem] w-10 bg-gradient-to-r from-cyan-500 to-blue-500 ... rounded-xl"
           style={{ left: "50%", x: "-50%", top: "50%", y: "-50%" }}
         />
         <motion.span
-         
           className="absolute h-1 w-5 bg-gradient-to-r from-cyan-500 to-blue-500 ... rounded-xl"
           style={{
             x: "-50%",
@@ -47,37 +44,4 @@ const AnimatedHamburgerButton = () => {
       </motion.button>
     </MotionConfig>
   );
-};
-
-const VARIANTS = {
-  top: {
-    open: {
-      rotate: ["0deg", "0deg", "45deg"],
-      top: ["35%", "50%", "50%"],
-    },
-    closed: {
-      rotate: ["45deg", "0deg", "0deg"],
-      top: ["50%", "50%", "35%"],
-    },
-  },
-  middle: {
-    open: {
-      rotate: ["0deg", "0deg", "-45deg"],
-    },
-    closed: {
-      rotate: ["-45deg", "0deg", "0deg"],
-    },
-  },
-  bottom: {
-    open: {
-      rotate: ["0deg", "0deg", "45deg"],
-      bottom: ["35%", "50%", "50%"],
-      left: "50%",
-    },
-    closed: {
-      rotate: ["45deg", "0deg", "0deg"],
-      bottom: ["50%", "50%", "35%"],
-      left: "calc(50% + 10px)",
-    },
-  },
 };
