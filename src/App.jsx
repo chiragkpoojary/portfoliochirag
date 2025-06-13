b79e1ad92a48dd4aab3442c5bb01357acee89b54
chiragkpoojary <chiragkpoojary30@gmail.com>
2025-06-13 11:48:30 +0530

    reloading problem

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,14 @@ import Project from './Components/Projects';
 import LetsConnect from './Components/LetsConnect';
 import Footer from './Components/Footer';
 import Quote from './Components/Quote';
+window.addEventListener("load", () => {
+  if (window.location.hash) {
+    history.replaceState(null, "", window.location.pathname);
+  window.scrollTo(0, 0);
+  }
+});
+
+
 function App() {
   return (
     <div >
